ab0bf95b2da8e35f3e4811ee4312396c1a3b9623
Magnus Brange <magnus@brange.nu>
2020-11-21 22:19:30 +0100

    API for fetching input

---
+++ utils/api.py
@@ -0,0 +1,42 @@
+import os
+import urllib.request
+import urllib.parse
+
+
+def _get_input_file_name(day):
+    input_dir = "{}/input".format(os.getcwd())
+    if not os.path.exists(input_dir):
+        os.makedirs(input_dir)
+    input_file = "{}/day_{}".format(input_dir, day)
+    return input_file
+
+
+def _check_input_file(day):
+    input_file = _get_input_file_name(day)
+    if os.path.exists(input_file):
+        f = open(input_file, "r")
+        print("Reusing data from {}".format(input_file))
+        return f.read()
+    return None
+
+
+def fetch(year, day):
+    cached = _check_input_file(day)
+    if cached:
+        return cached
+
+    sid = os.environ.get('aoc_sessionid')
+
+    url = 'https://adventofcode.com/{}/day/{}/input'.format(year, day)
+
+    headers = {'cookie': 'session={}'.format(sid)}
+
+    data = urllib.parse.urlencode({})
+    data = data.encode('ascii')
+    req = urllib.request.Request(url, data, headers)
+    with urllib.request.urlopen(req) as response:
+        resp = response.read().decode('utf-8').strip()
+    f = open(_get_input_file_name(day), "w")
+    f.write(resp)
+    f.close()
+    return resp
