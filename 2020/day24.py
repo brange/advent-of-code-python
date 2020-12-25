66983369a0cdbf24f4772688e9b2b7f9e7796628
Magnus Brange <magnus@brange.nu>
2020-12-25 08:23:39 +0100

    Improve 2020 day24

--- 2020/day24.py
+++ 2020/day24.py
@@ -30,33 +30,37 @@ def part1(data):
 def calculate_color(tile, tiles):
     num_black = 0
     color = tiles.get(tile, 0)
+    missing_tiles = set()
     for (x_diff, y_diff) in DIRECTIONS.values():
         key_ = (tile[0] + x_diff, tile[1] + y_diff)
-        num_black += tiles.get(key_, 0)
+        if key_ in tiles:
+            num_black += tiles[key_]
+        else:
+            missing_tiles.add(key_)
 
     # Any black tile with zero or more than 2 black tiles immediately adjacent to it is flipped to white.
     # Any white tile with exactly 2 black tiles immediately adjacent to it is flipped to black.
     if color and (num_black == 0 or num_black > 2):
-        return 0
+        return 0, missing_tiles
     elif not color and num_black == 2:
-        return 1
-    return None
+        return 1, missing_tiles
+    return None, missing_tiles
 
 
 def part2(tiles):
     for day in range(100):
         changed_tiles = {}
-        for (x, y) in tiles.keys():
-            for (x_diff, y_diff) in DIRECTIONS.values():
-                key_ = (x + x_diff, y + y_diff)
-                if key_ not in tiles:
-                    if calculate_color(key_, tiles):
-                        changed_tiles[key_] = 1
-
+        missing_tiles = set()
         for tile, color in tiles.items():
-            next_color = calculate_color(tile, tiles)
+            next_color, missing = calculate_color(tile, tiles)
+            missing_tiles.update(missing)
             if next_color is not None:
                 changed_tiles[tile] = next_color
+
+        for tile in missing_tiles:
+            color = calculate_color(tile, tiles)[0]
+            if color is not None:
+                changed_tiles[tile] = color
 
         for key, value in changed_tiles.items():
             tiles[key] = value
